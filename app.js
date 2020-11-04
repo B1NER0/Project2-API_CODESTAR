32add7db8291344eefb6b55879271e321b2a49f1
B1NER0 <70958872+B1NER0@users.noreply.github.com>
2020-11-04 14:50:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.get('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const { json } = require('express');
+
+const url = 'mongodb://username:password@3.84.36.77/thisOne?retryWrites=true&w=majority'; //
+
+
+
+app.get('/', (req, res) => {
+    //res.send("This is the homepage baby OOOH hhYEEAH");
+
+    MongoClient.connect(url, (err, database) => {
+        if (err) return console.log(err);
+       var db = database.db("thisOne");
+        db.collection("Test").findOne({
+           _id: "1"
+        },
+        function(err, result){
+            if(err) return console.log(err);
+            res.send(result.name);
+            console.log(result.name);             
+    });
+});
+});
+app.listen(PORT, () => {
+    console.log("Server is running...");
+
 });
 
-app.post('/', function(req, res) {
-  res.send({
-    "Output": "Hello World!"
-  });
-});
-
-
-// Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
